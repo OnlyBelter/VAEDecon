78e5efb6ef7b417d1d0d4e0f3d09177514be826f
OnlyBelter <xiongxin20008@126.com>
2023-09-10 19:39:31 +0800

    fix bug

--- vaedecon/decon_cf/deside.py
+++ vaedecon/decon_cf/deside.py
@@ -172,8 +172,8 @@ class DeSide(object):
             if pathway_mask is not None:
                 if input_gene_list == "intersection_with_pathway_genes":
                     gep_gene_list = [i for i in x_obj.exp.columns.to_list() if i in pathway_mask.index.to_list()]
-                elif input_gene_list == 'filtered_genes':
-                    assert filtered_gene_list is not None, 'filtered_gene_list should not be None'
+                elif input_gene_list == 'filtered_genes' and filtered_gene_list is not None:
+                    # assert filtered_gene_list is not None, 'filtered_gene_list should not be None'
                     gep_gene_list = filtered_gene_list.copy()
                 else:
                     gep_gene_list = x_obj.exp.columns.to_list()
