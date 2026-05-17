b2f52881ce67cfdcdac0b7f6178d17303625fea2
OnlyBelter <onlybelter@gmail.com>
2026-05-17 17:32:09 +0800

    chore: restore metrics list and annotate hierarchy codes

--- vaedecon/utility/hierarchical_encoding.py
+++ vaedecon/utility/hierarchical_encoding.py
@@ -18,20 +18,20 @@ SORTED_CELL_TYPES = [
 ]
 
 HIERARCHICAL_ENCODING = {
-    "NK": [1, 0, 0, 0, 0, 0, 0, 0],
-    "Non-plasma B cells": [1, 0, 0, 0, 1, 0, 0, 0],
-    "Plasma B cells": [1, 0, 0, 0, 1, 0, 0, 0],
-    "CD4 T": [1, 0, 0, 0, 0, 1, 0, 0],
-    "CD8 T (GZMK high)": [1, 0, 0, 0, 0, 1, 0, 0],
-    "CD8 T effector": [1, 0, 0, 0, 0, 1, 0, 0],
-    "Double-neg-like T": [1, 0, 0, 0, 0, 1, 0, 0],
-    "Monocytes": [0, 1, 0, 0, 0, 0, 1, 0],
-    "Macrophages": [0, 1, 0, 0, 0, 0, 1, 0],
-    "DC": [0, 1, 0, 0, 0, 0, 0, 0],
-    "Mast Cells": [0, 1, 0, 0, 0, 0, 0, 0],
-    "Neutrophils": [0, 1, 0, 0, 0, 0, 0, 0],
-    "Endothelial Cells": [0, 0, 1, 0, 0, 0, 0, 0],
-    "CAFs": [0, 0, 1, 0, 0, 0, 0, 1],
-    "Myofibroblasts": [0, 0, 1, 0, 0, 0, 0, 1],
-    "Cancer Cells": [0, 0, 0, 1, 0, 0, 0, 0],
+    "NK":                 [1, 0, 0, 0, 0, 0, 0, 0],  # Lymphoid cells
+    "Non-plasma B cells": [1, 0, 0, 0, 1, 0, 0, 0],  # B cells
+    "Plasma B cells":     [1, 0, 0, 0, 1, 0, 0, 0],
+    "CD4 T":              [1, 0, 0, 0, 0, 1, 0, 0],  # T cells
+    "CD8 T (GZMK high)":  [1, 0, 0, 0, 0, 1, 0, 0],
+    "CD8 T effector":     [1, 0, 0, 0, 0, 1, 0, 0],
+    "Double-neg-like T":  [1, 0, 0, 0, 0, 1, 0, 0],
+    "Monocytes":          [0, 1, 0, 0, 0, 0, 1, 0],  # Monocytic cells
+    "Macrophages":        [0, 1, 0, 0, 0, 0, 1, 0],
+    "DC":                 [0, 1, 0, 0, 0, 0, 0, 0],  # Myeloid cells
+    "Mast Cells":         [0, 1, 0, 0, 0, 0, 0, 0],
+    "Neutrophils":        [0, 1, 0, 0, 0, 0, 0, 0],
+    "Endothelial Cells":  [0, 0, 1, 0, 0, 0, 0, 0],  # Stromal cells
+    "CAFs":               [0, 0, 1, 0, 0, 0, 0, 1],  # Fibroblasts
+    "Myofibroblasts":     [0, 0, 1, 0, 0, 0, 0, 1],
+    "Cancer Cells":       [0, 0, 0, 1, 0, 0, 0, 0],
 }
